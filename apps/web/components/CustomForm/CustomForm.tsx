0a244f925fd96c5b7932650cd8962e10a0a93a1b
Ivan Matskevich <faxtalk666@gmail.com>
2023-11-22 20:32:59 +0300

    fix: improve button

--- apps/web/components/CustomForm/CustomForm.tsx
+++ apps/web/components/CustomForm/CustomForm.tsx
@@ -1,61 +1,67 @@
-import { useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 
 import { apiUrl } from '@/config/config';
 import DynamicButton from '@/components/DynamicButton';
-
-const getColor = (status: number) => {
-  if (status === 200) {
-    return 'green';
-  } else if (status >= 400) {
-    return 'red';
-  } else {
-    return '#ccc';
-  }
-};
+import getColor from '@/utils/getColor';
+import styles from './CustomForm.module.css'
 
 const CustomForm = () => {
-  const [serverResponse, setServerResponse] = useState<any>(null);
+  const [serverResponse, setServerResponse] = useState<{ status?: number | undefined; error?: { message?: string } }>({});
+  const [loading, setLoading] = useState<boolean>(false);
+  const [color, setColor] = useState<string>(getColor(0, true));
+  const [text, setText] = useState<string | undefined>(undefined);
+  const [inputValue, setInputValue] = useState<string | undefined>(undefined);
 
-  async function handleSubmit(event: { preventDefault: () => void; }) {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     try {
+      setLoading(true);
+
       const response = await fetch(`${apiUrl}/api/getData`, {
         method: 'POST',
+        body: inputValue,
       })
 
-      const data = await response.json();
+      const data = await response?.json();
       setServerResponse(data);
+
+      setColor(getColor(response?.status, true));
+      setText(response?.status === 200 ? 'Success' : 'Error');
+      setTimeout(() => {
+        setColor(getColor(0, true));
+        setText(undefined);
+      }, 1000);
     } catch (error) {
       console.error(error);
-      setServerResponse({status: 500, error: {message: 'Internal Server Error'}});
+      setServerResponse({status: 500, error: { message: 'Internal Server Error' }});
+    } finally {
+      setLoading(false);
     }
   }
+  let inputClasses = `${styles.input}`;
+  if (loading) inputClasses += ` ${styles.loading}`;
+  if (serverResponse?.status === 200) inputClasses += ` ${styles.success}`;
+  if (serverResponse?.status === 500) inputClasses += ` ${styles.error}`;
+  if (serverResponse?.status !== 500 && serverResponse?.status !== 200) inputClasses += ` ${styles.common}`;
   return (
     <form
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        padding: 50,
-        position: 'relative',
-        borderRadius: '12px !important'
-      }}
+      onSubmit={handleSubmit}
+      className={styles.form}
     >
       <input
         type="text"
-        style={{
-          padding: '10px',
-          marginRight: '10px',
-          borderRadius: '5px',
-          border: `2px solid ${getColor(serverResponse?.status)}`,
-          fontSize: '16px',
-        }}
+        className={inputClasses}
         placeholder="Enter text"
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <DynamicButton serverResponse={serverResponse} onSubmit={handleSubmit} />
+      <DynamicButton
+        buttonColor={color as 'error' | 'common' | 'success' | undefined}
+        loading={loading}
+        buttonText={text}
+      />
       {serverResponse?.error?.message && (
-        <div style={{ position: 'absolute', bottom: 20, left: 'auto', color: 'red'}}>{serverResponse.error.message}</div>
+        <div className={styles.errorMessage}>{serverResponse.error.message}</div>
       )}
     </form>
   )
--- apps/web/components/DynamicButton/DynamicButton.tsx
+++ apps/web/components/DynamicButton/DynamicButton.tsx
@@ -1,47 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { MouseEventHandler } from 'react';
 
-const DynamicButton = ({ serverResponse, onSubmit }: { serverResponse: { status: number}, onSubmit: Function }) => {
-  const [loading, setLoading] = useState(false);
-  const [buttonStyle, setButtonStyle] = useState({});
-console.log({serverResponse})
-  useEffect(() => {
-    if (serverResponse?.status === 200) {
-      setButtonStyle({ backgroundColor: 'green' });
-    } else if (serverResponse?.status >= 400) {
-      setButtonStyle({ backgroundColor: 'red' });
-    } else {
-      setButtonStyle({});
-    }
-  }, [serverResponse]);
+import styles from './DynamicButton.module.css';
 
-  const handleClick = async (event: any) => {
-    try {
-      setLoading(true);
-      await onSubmit(event);
-    } catch (error) {
-      console.error('Form submit error:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+const DynamicButton = ({
+  buttonColor,
+  loading,
+  onClick,
+  buttonText,
+} : {
+  buttonColor?: 'error' | 'success' | 'common' | undefined,
+  loading?: boolean,
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined,
+  buttonText?: string,
+}) => {
+
+  const buttonColorClass = buttonColor ? styles[buttonColor] : styles.common;
+  const isLoading = loading || buttonColor === 'success' || buttonColor === 'error';
 
   return (
     <button
-      style={{
-        ...buttonStyle,
-        padding: '10px',
-        borderRadius: '5px',
-        border: '1px solid #007BFF',
-        background: '#007BFF',
-        color: '#fff',
-        fontSize: '16px',
-        cursor: 'pointer',
-        width: '100px',
-      }}
-      onClick={handleClick}
-      disabled={loading}
+      className={`${styles.button} ${buttonColorClass} ${isLoading ? styles.loading : ''}`}
+      onClick={onClick}
+      type='submit'
+      disabled={loading || buttonColor === 'success' || buttonColor === 'error'}
     >
-      {loading ? 'Loading...' : 'Submit'}
+      {buttonText? buttonText : (loading ? 'Loading...' : 'Submit')}
     </button>
   );
 };
